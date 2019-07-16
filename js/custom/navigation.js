4179f519990828cf6f28a5c7ff70a953379115e3
alvarorubioc <alvaro.rubiocamino@gmail.com>
2019-07-16 16:56:59 +0200

    product page 1.1

--- js/custom/navigation.js
+++ js/custom/navigation.js
@@ -1,7 +1,18 @@
-function openFilters() {
-   var element = document.getElementById("sh-products-subcategory");
-   element.classList.toggle("is-open");
-}
+$("#open-filters").click(function (e) {
+  e.stopPropagation();
+  $("#sh-products-subcategory").toggleClass("is-open");
+  if ($(window).width() < 769) {
+    $("body").addClass("mm--open");
+  }
+});
+
+$('#close-aside-mobile').click(function (e) {
+  if ($("#sh-products-subcategory").toggleClass('is-open')) {
+    $("#sh-products-subcategory").removeClass("is-open");
+    $("body").removeClass("mm--open");
+  }
+});
+
 
 function openFiltersSearch() {
   var element = document.getElementById("wrap-search");
@@ -14,39 +25,39 @@ gridButton = $('button.grid-view');
 wrapper = $('div.sh-products__grid');
 
 
-listButton.on('click',function(){
+listButton.on('click', function () {
   gridButton.removeClass('active');
   listButton.addClass('active');
   wrapper.removeClass('sh-products__grid').addClass('sh-products__list');
-  
+
 });
 
-gridButton.on('click',function(){
+gridButton.on('click', function () {
   listButton.removeClass('active');
   gridButton.addClass('active');
   wrapper.removeClass('sh-products__list').addClass('sh-products__grid');
-  
+
 });
 
 
 $(function () {
   $(document).scroll(function () {
-	  var $nav = $(".sh-menu");
-	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-	});
+    var $nav = $(".sh-menu");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
 });
 
 
-$(document).ready(function() {
-	if ($(window).width() < 768) {
-		$('#sh-navig').remove();
-	}
+$(document).ready(function () {
+  if ($(window).width() < 768) {
+    $('#sh-navig').remove();
+  }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-	if ($(window).width() > 769) {
-	
-	$('#menu-mobi').prop('id','nuevaID');
-	}
+  if ($(window).width() > 769) {
+
+    $('#menu-mobi').prop('id', 'nuevaID');
+  }
 });
